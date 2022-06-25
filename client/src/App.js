3a0367c3379c3caf31d145f870e31316bc425cba
Ankit Goyala <ankitgoyala2@gmail.com>
2022-06-26 02:42:45 +0545

    added client modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import AddClientModal from "./components/AddClientModal";
 import Client from "./components/Client";
 
 import Header from "./components/Header";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -14,6 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Client />
         </div>
       </ApolloProvider>
